a6baaa3a0001874592294b859ef7f5011cc0973a
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-03-03 21:15:15 -0800

    Division and Loops .py files

--- Python/If-Else.py
+++ Python/If-Else.py
@@ -9,7 +9,7 @@ def isEven(test_number):
 
 def inDefinedLimit(test_number):
     return test_number in range(2, 6) or test_number > 20
-    # Definied limit is between 2 to 6 or greater than 20
+    # Defined limit is between 2 to 6 or greater than 20
 
 if __name__ == '__main__':
     n = int(input().strip())
---
+++ Python/Int vs. Float Division.py
@@ -0,0 +1,17 @@
+"""
+Created 3/3/2018
+
+@author: acfromspace
+"""
+
+def intDivision(a, b):
+    return a // b
+    
+def floatDivision(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(intDivision(a,b), floatDivision(a,b), sep = "\n")
