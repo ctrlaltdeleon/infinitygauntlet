f56b494f613b9557482ceeea19686c8d49e3e800
AC De Leon <aadeleon@sdsu.edu>
2018-10-02 14:49:03 -0700

    [*] Stripped .css, sample code

---
+++ app/src/ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Ninjas extends Component {
+  render() {
+    return (
+      <div className="ninja">
+        <div>Name: AC</div>
+        <div>Age: 5</div>
+        <div>You are beautiful</div>
+      </div>
+    );
+  }
+}
+
+export default Ninjas;
