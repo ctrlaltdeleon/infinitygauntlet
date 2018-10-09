b6e8e2dae83bf08ab53f14738347f641d1a27655
AC De Leon <aadeleon@sdsu.edu>
2018-10-08 19:06:20 -0700

    [+] Post prettify

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Pokeball from "../pokeball.png";
 
 class Home extends Component {
   state = {
@@ -17,13 +18,15 @@ class Home extends Component {
   }
   render() {
     const { posts } = this.state;
+    // post.length checks if there's any post to begin with
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="Pokeball" />
             <div className="card-content">
               <Link to={"/" + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -34,7 +37,7 @@ class Home extends Component {
       <div className="center">No posts yet!</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
