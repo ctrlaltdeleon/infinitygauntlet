f3f9657f5dc86facd8a1d5c9b901b4a0c4672403
AC De Leon <aadeleon@sdsu.edu>
2018-10-08 17:32:48 -0700

    [+] Axios! REST API

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>This is a paragraph.</p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet!</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>{postList}</p>
+      </div>
+    );
+  }
+}
 
 export default Home;
