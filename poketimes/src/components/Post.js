acc6b38b5933f3fc39ef4fadc0246b71f4e0e4dd
AC De Leon <aadeleon@sdsu.edu>
2018-10-11 16:28:38 -0700

    [+] Post routing via redux

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,27 +1,14 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
+import { connect } from "react-redux";
 
 class Post extends Component {
-  state = {
-    post: null
-  };
-
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      this.setState({
-        // We called it "res" from the above arrow function, data is just the data class for the object
-        post: res.data
-      });
-    });
-  }
-
   render() {
-    console.log(this.state);
-    const post = this.state.post ? (
+    console.log(this.props);
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading posts...</div>
@@ -30,4 +17,11 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => post.id === id)
+  };
+};
+
+export default connect(mapStateToProps)(Post);
