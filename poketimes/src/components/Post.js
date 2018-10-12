593ab1dd2b28ed3f6bb92dbdaf4a077673684297
AC De Leon <aadeleon@sdsu.edu>
2018-10-11 17:02:00 -0700

    [+] actionCreators

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import axios from "axios";
 import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions";
 
 class Post extends Component {
   handleClick = () => {
@@ -36,9 +37,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    deletePost: id => {
-      dispatch({ type: "DELETE_POST", id: id });
-    }
+    deletePost: id => dispatch(deletePost(id))
   };
 };
 
