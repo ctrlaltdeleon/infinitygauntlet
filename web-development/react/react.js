60b4061b77807d8fb53d7437aec9d655d0ec6fcd
AC De Leon <aadeleon@sdsu.edu>
2018-10-26 19:07:19 -0700

    [+] OOF (1, react.js standard component)

---
+++ web-development/react/react.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class react extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Standard component class</h1>
+      </div>
+    );
+  }
+}
+
+export default react;
