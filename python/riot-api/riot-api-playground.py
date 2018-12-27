03aa0c565ff2a09af79f3dbd6ab84adfe5a55625
AC De Leon <aadeleon@sdsu.edu>
2018-12-26 17:14:39 -0800

    [+] Riot-API (Phase 1)

--- python/riot-api/riot-api-playground.py
+++ python/riot-api/riot-api-playground.py
@@ -2,6 +2,7 @@
 # @filename     riot-api-playground.py
 # @description  Riot Games API playground
 # @notes        Riot Games API Reference: https://developer.riotgames.com/api-methods/
+# @todo         Consider separating main() into more functions, considering if the player plays ranked
 
 # "requests" library installed using "pip"
 import requests
@@ -19,8 +20,8 @@ def requestSummonerV4(region, summonerName, APIKey):
 
 def requestLeagueV4(region, ID, APIKey):
     # LEAGUE-V4
-    URL = "https://" + region + "api.riotgames.com/lol/league/v4/positions/by-summoner/" + \
-        ID + "/entry?api_key=" + APIKey
+    URL = "https://" + region + ".api.riotgames.com/lol/league/v4/positions/by-summoner/" + \
+        ID + "?api_key=" + APIKey
     # Goes to the URL and retrieves the .json file
     response = requests.get(URL)
 
@@ -35,13 +36,13 @@ def main():
     # Ask the user for 3 things, their region, summoner name, and API Key.
 
     region = input('Choose a region (Type "na1" for North America): ')
-    summonerName = input("Summoner name: ")
+    summonerName = input("Summoner name (Must be ranked): ")
     APIKey = input('Copy and paste your API Key here: ')
 
     # Run the function to
     summonerV4Payload = requestSummonerV4(region, summonerName, APIKey)
-    # ID = str(summonerV4Payload["id"])
-    # leagueV4Payload = requestLeagueV4(region, ID, APIKey)
+    encryptedSummonerID = str(summonerV4Payload["id"])
+    leagueV4Payload = requestLeagueV4(region, encryptedSummonerID, APIKey)
 
     # Output
     print("\n*************************")
@@ -49,7 +50,11 @@ def main():
     print("*************************\n")
     print("Name: " + str(summonerV4Payload["name"]))
     print("Level: " + str(summonerV4Payload["summonerLevel"]))
-    # print(str(leagueV4Payload))
+    print("League Name: " + str(leagueV4Payload[0]["leagueName"]))
+    print("Tier: " + str(leagueV4Payload[0]["tier"]))
+    print("Ranked Wins: " + str(leagueV4Payload[0]["wins"]))
+    print("Ranked Losses: " + str(leagueV4Payload[0]["losses"]))
+    print("League Points: " + str(leagueV4Payload[0]["leaguePoints"]))
 
 
 if __name__ == "__main__":
