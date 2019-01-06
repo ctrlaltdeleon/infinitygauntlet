7d0e19f878fcc00ef1ed7128108f8e9aa6ac44a6
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-01-05 21:15:00 -0800

    [+] Java Practice

---
+++ java/non-repeating-char.java
@@ -0,0 +1,27 @@
+// Function for java to detect if there's a non-repeating character
+
+public static char uniqueChar(String a)
+{
+    int repeatCount = 0;
+    char [] stringCharArray = a.toCharArray();
+    
+    for(int i = 0; i < stringCharArray.length; i++)
+    {
+        repeatCount = 0;
+        
+        for(int j = 0; j < stringCharArray.length; j++)
+        {
+            if(stringCharArray[i] == stringCharArray[j] && i != j)
+            {
+                repeatCount = 1;
+                break;
+            }
+        }
+        
+        if(repeatCount == 0)
+            return stringCharArray[i];
+    }
+    
+    return 0;
+    
+}
--- other/password-files.bat
+++ other/password-files.bat
@@ -1,10 +1,12 @@
+REM File to unpack data is the correct password is given.
+
 @echo off
 
 set key=*****
 
 title ::
 color 0a
-echo Please enter your password.
+echo Please enter your password:
 set/p "pass= >"
 if %pass%==%key% goto UnPack
 cls
@@ -20,7 +22,7 @@ echo That is the correct passord!
 echo.
 echo Your data will unpacked and sent here: %dest%
 echo.
-echo Please make sure this file does not already exist befor continuing
+echo Please make sure this file does not already exist before continuing
 echo as this may ruin your data.
 echo.
 pause
