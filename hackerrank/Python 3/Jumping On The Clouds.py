5ad0aec0dd817b6f25eacb5bd2fccfcb8e759f2e
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-01-07 10:36:51 -0800

    [+] Hackerrank Jumping On The Clouds

---
+++ hackerrank/Python 3/Jumping On The Clouds.py
@@ -0,0 +1,36 @@
+"""
+Created 1/6/2019
+
+@author: acfromspace
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+
+
+def jumpingOnClouds(c):
+
+    # from the list, map everything into a strings then join them together into 1 string
+    journey = ''.join(c)
+    # journey = ''.join(map(str, c))
+    return journey.count('00') + journey.count('1')
+
+
+if __name__ == '__main__':
+
+    # n = int(input("Input number of clouds: "))
+
+    c = input("Input thunder (1) vs cumulus (0): ")
+    # c = list(map(int, input().rstrip().split()))
+
+    print("Number of jumps:", jumpingOnClouds(c))
+
+# 7
+# 0 0 1 0 0 1 0
