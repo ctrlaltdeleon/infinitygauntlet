d71247807f74c9995ce454eb3b4d4cdddbc02c60
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-01-06 18:05:10 -0800

    [+] Fixed Python files, exercise, PA&DS 1.10 Control Structures

---
+++ python/fizzbuzz.py
@@ -0,0 +1,16 @@
+class FizzBuzz:
+
+    def output(self):
+        for i in range(1, 101):
+            string = ""
+            if i % 3 == 0:
+                string += "Fizz"
+            if i % 5 == 0:
+                string += "Buzz"
+            print(i, string)
+
+
+# create object from class solution
+test_object = FizzBuzz()
+
+test_object.output()
--- python/playground.py
+++ python/playground.py
@@ -1,13 +1,13 @@
 class Solution:
 
     def counting(self, number):
-        for i in number:
+        for i in range(1, number+1):
             print(i)
 
 
 # create object from class solution
 test_object = Solution()
 
-number = int(10)
+number = 10
 
 test_object.counting(number)
