a7ff50178288be8b19866365a7d9d7a938eba3c9
AC De Leon <aadeleon@sdsu.edu>
2019-01-23 17:35:52 -0800

    [+] Hackerrank Sum Prod + playground.py

---
+++ hackerrank/Python 3/Sum Prod.py
@@ -0,0 +1,12 @@
+"""
+@author: acfromspace
+"""
+
+import numpy
+
+if __name__ == "__main__":
+
+    # Input for a 2-d array
+    N, M = map(int, input().split())
+    A = numpy.array([input().split() for counter in range(N)], int)
+    print(numpy.prod(numpy.sum(A, axis=0), axis=None))
