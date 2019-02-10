2cb05b33cefc4039dadc64eb6332da39e45ef383
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-02-10 13:23:10 -0800

    [+] Hackerrank Day 10 Binary Numbers (Python)

---
+++ hackerrank/python/30-days-of-code/day-10-binary-numbers.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+
+    print(len(max(bin(int(input().strip()))[2:].split('0'))))
+
+# int(input().strip())
+# take the input and strip any unneeded characters from beginning or end
+# 13
+
+# bin()
+# take the input and transform to binary output
+# 0b1101
+
+# max([2:].split('0'))
+# starting from the 2nd index and onward (we don't want the '0b') --> max('1101'.split('0'))
+# remove all '0' in the string --> max('11','1')
+# max looks for the biggest value which is '11' --> '11'
+
+# len('11')
+# the length of the string is '2' so we print out --> 2
