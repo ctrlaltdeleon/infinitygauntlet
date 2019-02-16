e7275ac290c9cead047e52f4f0af84e027dad26c
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-02-16 11:09:57 -0800

    [+] Firecode bubble-sort.py (in /data structures)

--- algorithms/bubble-sort.py
+++ algorithms/bubble-sort.py
@@ -1,30 +1,54 @@
-# This version is more efficient than standard due to checking if sorted or not
+# Standard O(n^2) complexity solution
+def bubbleSort1(data):
+    for i in range(len(data)-1, 0, -1):
+        for j in range(i):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+    return data
 
 
-def bubbleSort(data):
+def bubbleSort2(data):
+    # This version is more efficient than standard due to checking if sorted or not
 
+    # lastIndex is here to keep check of amount of passes, not needed for bubble sort
     lastIndex = len(data) - 1
     # Checks last step to see if it was the last needed step
-    earlyExit = False
+    isSorted = False
 
-    while lastIndex > 0 and not earlyExit:
-        for index in range(lastIndex):
-            if data[index] > data[index + 1]:
-                data[index], data[index + 1] = data[index + 1], data[index]
-                earlyExit = False
+    while lastIndex > 0 and not isSorted:
+        for i in range(lastIndex):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+                isSorted = False
             else:
-                earlyExit = True
+                isSorted = True
         lastIndex -= 1
 
-    print("Early Exit: ", earlyExit, " | ", len(data) - lastIndex, " Passes")
+    print("Early Exit: ", isSorted, " | ", len(data) - lastIndex, " Passes")
 
 
-testList = [9, 5, 1]
+def bubbleSort3(data):
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for i in range(len(data)-1):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                isSorted = False
+    return data
 
+
+testList = [9, 5, 1, 3, 6, -2, -8]
 print(testList)
-bubbleSort(testList)
+bubbleSort1(testList)
 print(testList, "\n")
 
+testList = [9, 5, 1, 3, 6, -2, -8]
 print(testList)
-bubbleSort(testList)
+bubbleSort2(testList)
+print(testList, "\n")
+
+testList = [9, 5, 1, 3, 6, -2, -8]
+print(testList)
+bubbleSort3(testList)
 print(testList)
