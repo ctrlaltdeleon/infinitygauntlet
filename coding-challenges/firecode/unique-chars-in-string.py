d52fc8f19b4a74fa512c9ee1ac3dc0f5dd48133c
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-02-23 16:40:57 -0800

    [+] /firecode better-fibonacci.py + unique-chars-in-string.py

---
+++ coding-challenges/firecode/unique-chars-in-string.py
@@ -0,0 +1,11 @@
+"""
+@author: acfromspace
+"""
+
+
+def unique_chars_in_string(input_string):
+    return len(set(input_string)) == len(input_string)
+
+
+input_string = str(input("Input a string: "))
+print("unique_chars_in_string():", unique_chars_in_string(input_string))
