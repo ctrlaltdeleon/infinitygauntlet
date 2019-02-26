024ad44ec253bf8fdaa3b803c9ad5d036c50bc43
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-02-26 00:00:32 -0800

    [+] /javascript/typescript

---
+++ javascript/typescript/index.ts
@@ -0,0 +1,53 @@
+// import * as _ from "lodash";
+
+// async function hello() {
+//   return "world";
+// }
+
+// console.log("hello world");
+
+// const url = new URL("...");
+
+// let lucky: any = 23;
+
+// lucky = "23";
+
+// type Style = "bold" | "italic";
+
+// let font: Style;
+
+// interface Person {
+//   first: string;
+//   last: string;
+//   [key: string]: any;
+// }
+
+// const person = {
+//   first: "Hello",
+//   last: "Name",
+// };
+
+// // FUNCTIONS
+// function pow(x: number, y: number): string {
+//   return Math.pow(x, y).toString();
+// }
+
+// pow(2, 4);
+
+// // ARRAYS
+// type MyList = [number?, string?, boolean?];
+
+// const arr: MyList = [];
+
+// arr.push(1);
+// arr.push(22);
+// arr.push(220);
+
+// // GENERICS
+// class Observable<T> {
+//   constructor(public value: T) {}
+// }
+
+// let x: Observable<number>;
+// let y: Observable<Person>;
+// let z = new Observable(23);
