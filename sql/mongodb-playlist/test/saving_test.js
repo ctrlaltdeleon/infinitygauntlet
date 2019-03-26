5528153c0b67c7862daa9248afcc53bdb9f00c63
AC De Leon <aadeleon@sdsu.edu>
2019-03-25 20:18:33 -0700

    [+] /sql/mongodb-playlist, saving data to mongodb

--- sql/mongodb-playlist/test/saving_test.js
+++ sql/mongodb-playlist/test/saving_test.js
@@ -5,13 +5,15 @@ const MarioChar = require("../models/mariochar");
 // describe tests
 describe("saving records", function() {
   // create tests
-  it("saves a record to the database", function() {
+  it("saves a record to the database", function(done) {
     var char = new MarioChar({
       name: "Bowser",
     });
     // asynchronous request
     char.save().then(function() {
-      assert(char.isNew);
+      assert(char.isNew === false);
+      // reference function to the "it" parameter
+      done();
     });
   });
 });
