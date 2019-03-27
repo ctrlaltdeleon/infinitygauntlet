c2261a8f9584c6aedab761d48335f45a80e8d950
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-03-27 00:43:11 -0700

    [*] /sql/mongodb-playlist, es6 promises

--- sql/mongodb-playlist/test/connection.js
+++ sql/mongodb-playlist/test/connection.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 
-// connect to mongodb
-mongoose.connect("mongodb://localhost/testaroo");
+// es6 promises to be used for the promise library
+mongoose.Promise = global.Promise;
 
-// to use:
-// node connection.js
-mongoose.connection
-  .once("open", function() {
-    console.log("Connection to database successful.");
-  })
-  .on("error", function(error) {
-    console.log("Connection error!", error);
-  });
+// connect to the database before npm run test
+before(function(done) {
+  // connect to mongodb
+  mongoose.connect("mongodb://localhost/testaroo", { useNewUrlParser: true });
+
+  // to use:
+  // node connection.js
+  mongoose.connection
+    .once("open", function() {
+      console.log("Connection to database successful.");
+      done();
+    })
+    .on("error", function(error) {
+      console.log("Connection error!", error);
+    });
+});
--- sql/mongodb-playlist/test/saving_test.js
+++ sql/mongodb-playlist/test/saving_test.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 const MarioChar = require("../models/mariochar");
 
 // describe tests
-describe("saving records", function() {
+describe("describe: saving records...", function() {
   // create tests
   it("saves a record to the database", function(done) {
     var char = new MarioChar({
