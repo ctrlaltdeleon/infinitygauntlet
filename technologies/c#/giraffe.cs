b06b267c966140c112a13665e10eea16ed325eab
AC De Leon <acfromspace.deleon@gmail.com>
2019-05-25 12:47:10 -0700

    [+] ctechnologies/c#/giraffe.cs

---
+++ technologies/c#/giraffe.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 / 2); // Returns an integer.
+            Console.WriteLine(5 / 2.0); // Returns a decimal.
+
+
+            Console.ReadLine();
+        }
+    }
+}
