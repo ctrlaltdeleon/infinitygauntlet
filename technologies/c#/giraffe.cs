560f25cc1b822fb5de7320f2050c864b2fbb0c10
AC De Leon <acfromspace.deleon@gmail.com>
2019-05-26 10:37:02 -0700

    [+] ctechnologies/c#/giraffe.cs

--- technologies/c#/giraffe.cs
+++ technologies/c#/giraffe.cs
@@ -10,11 +10,24 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(5 / 2); // Returns an integer.
-            Console.WriteLine(5 / 2.0); // Returns a decimal.
+            string phrase = "nice one";
+            Console.WriteLine(phrase[0]); // "n".
+            Console.WriteLine(phrase.IndexOf("one")); // Finds the first occurrence, which is at 5.
+
+            int num = 5;
+            Console.WriteLine(num / 2); // Returns an integer.
+            Console.WriteLine(num / 2.0); // Returns a decimal.
+            Console.WriteLine(Math.Abs(-num)); // Calling methods.
+            Console.WriteLine(Math.Max(num, num + 5)); // Finds the max number between 2 parameters.
 
 
             Console.ReadLine();
+        }
+        public static string Reverse(string phrase)
+        {
+            char[] charArray = phrase.ToCharArray(); // Load string to character array.
+            Array.Reverse(charArray); // Reverse the array.
+            return new string(charArray); // Return the string.
         }
     }
 }
