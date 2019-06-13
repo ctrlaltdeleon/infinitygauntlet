9baac74b25bb8530a42f3ceba0c43ec3cd140e42
AC De Leon <acfromspace.deleon@gmail.com>
2019-06-13 12:29:58 -0700

    [-] technologies/c#/

--- technologies/c#/arrays.cs
+++ technologies/c#/arrays.cs
@@ -5,8 +5,10 @@ class Driver
     static void Main(string[] args)
     {
         int[] luckyNumbers = { 4, 8, 15, 42, 69, 420 };
+        double[] moneyAmounts = { 1.00, 2.00, 4.20 };
         string[] friends = new string[5]; // Creating an empty allocation for an array of strings.
         Console.WriteLine(luckyNumbers[5]);
+        Console.WriteLine(moneyAmounts[2]);
 
         Console.ReadLine();
     }
