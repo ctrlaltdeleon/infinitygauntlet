d8403a986951b457648b4c71fe03476e95936bae
AC De Leon <acfromspace.deleon@gmail.com>
2019-08-29 22:52:16 -0700

    [+] hackerrank/python/ 🌱

---
+++ coding-challenges/hackerrank/python/breaking-the-records.py
@@ -0,0 +1,20 @@
+"""
+@author: acfromspace
+"""
+
+
+def breakingRecords(scores):
+    mini = maxi = scores[0]
+    min_count = max_count = 0
+    for index in scores[1:]:
+        if index > maxi:
+            max_count += 1
+            maxi = index
+        if index < mini:
+            min_count += 1
+            mini = index
+    return max_count, min_count
+
+
+scores = [10, 5, 20, 4, 5, 25, 30, 10]
+print("breakingRecords():", breakingRecords(scores))
