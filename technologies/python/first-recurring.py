3247f2392fd9ce93631b2721277c4919f46edbf2
AC De Leon <acfromspace.deleon@gmail.com>
2019-10-03 16:33:07 -0700

    [+] technologies/python/ 🌱

---
+++ technologies/python/first-recurring.py
@@ -0,0 +1,17 @@
+"""
+@author: acfromspace
+"""
+
+
+def first_recurring(data):
+    seen = []
+    for index in data:
+        if index not in seen:
+            seen.append(index)
+        else:
+            return index
+    return "All unique characters."
+
+
+data = [1, 2, 3, 3, 2, 1, 3]
+print("first_recurring():", first_recurring(data))
