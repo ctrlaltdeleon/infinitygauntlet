5bcaa6bc8b439a0405db9451d21ff472140bb9bb
AC De Leon <acfromspace.deleon@gmail.com>
2019-10-10 18:56:46 -0700

    [*] technologies/arduino/ 🌱

--- technologies/arduino/mouse_bot.ino
+++ technologies/arduino/mouse_bot.ino
@@ -2,7 +2,9 @@
 
 void setup()
 {
+    Serial.begin(9600);
     SmartInventor.DCMotorUse();
+    randomSeed(analogRead(0));
 }
 
 void loop()
@@ -25,9 +27,24 @@ void loop()
     }
     else if (centerSensor < 500)
     {
-        // Turn left.
         SmartInventor.DCMotor(M1, CW, 50);
-        SmartInventor.DCMotor(M2, CW, 50);
+        SmartInventor.DCMotor(M2, CCW, 50);
+        delay(1000);
+        long randomTurn = random(100);
+        Serial.println(randomTurn);
+        if (randomTurn > 50)
+        {
+            // Turn left.
+            SmartInventor.DCMotor(M1, CW, 50);
+            SmartInventor.DCMotor(M2, CW, 50);
+        }
+        else if (randomTurn <= 50)
+        {
+            // Turn right.
+            SmartInventor.DCMotor(M1, CCW, 50);
+            SmartInventor.DCMotor(M2, CCW, 50);
+        }
+        delay(250);
     }
     else
     {
@@ -36,3 +53,17 @@ void loop()
         SmartInventor.DCMotor(M2, CW, 50);
     }
 }
+
+// 33 left
+// 2 left
+// 41 left
+// 77 right
+// 78 right
+// 55 right
+// 50 right
+// 97 right
+// 5 left
+// 16 left
+// 2 left
+// 60 right
+// 88 right
